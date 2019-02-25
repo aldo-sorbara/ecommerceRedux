0c208ae189bc92d5d0a8e6aae70defe3a7e7915e
aldo-sorbara <aldo1891@gmail.com>
2019-02-25 04:21:12 -0300

    feat(eslint-prettier): Add eslint config

--- src/common/actions/index.js
+++ src/common/actions/index.js
@@ -4,15 +4,15 @@ export const DECREMENT_COUNTER = 'DECREMENT_COUNTER';
 
 export const set = value => ({
   type: SET_COUNTER,
-  payload: value,
+  payload: value
 });
 
 export const increment = () => ({
-  type: INCREMENT_COUNTER,
+  type: INCREMENT_COUNTER
 });
 
 export const decrement = () => ({
-  type: DECREMENT_COUNTER,
+  type: DECREMENT_COUNTER
 });
 
 export const incrementIfOdd = () => (dispatch, getState) => {
--- src/server/index.js
+++ src/server/index.js
@@ -8,7 +8,7 @@ import { fetchCounter } from '../common/api/counter';
 import { renderToString } from 'react-dom/server';
 import serialize from 'serialize-javascript';
 import routes from '../common/routes';
-import api from  '../common/api';
+import api from '../common/api';
 
 const assets = require(process.env.RAZZLE_ASSETS_MANIFEST);
 
@@ -19,38 +19,41 @@ server
   .use(express.static(process.env.RAZZLE_PUBLIC_DIR))
   .use('/api', api)
   .get('/*', (req, res) => {
-  
-      // Compile an initial state
-      const preloadedState = { counter:  10 };
+    // Compile an initial state
+    const preloadedState = { counter: 10 };
 
-      // Create a new Redux store instance
-      const store = configureStore(preloadedState);
+    // Create a new Redux store instance
+    const store = configureStore(preloadedState);
 
-      // Render the component to a string
-      const markup = renderToString(
-        <Provider store={store}>
-          <StaticRouter location={req.url} context={{}}>
-            {renderRoutes(routes, preloadedState)}
-          </StaticRouter>
-        </Provider>
-      );
+    // Render the component to a string
+    const markup = renderToString(
+      <Provider store={store}>
+        <StaticRouter location={req.url} context={{}}>
+          {renderRoutes(routes, preloadedState)}
+        </StaticRouter>
+      </Provider>
+    );
 
-      // Grab the initial state from our Redux store
-      const finalState = store.getState();
+    // Grab the initial state from our Redux store
+    const finalState = store.getState();
 
-      res.send(`<!doctype html>
+    res.send(`<!doctype html>
     <html lang="">
     <head>
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta charSet='utf-8' />
         <title>Razzle Redux Example</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        ${assets.client.css
-          ? `<link rel="stylesheet" href="${assets.client.css}">`
-          : ''}
-          ${process.env.NODE_ENV === 'production'
-            ? `<script src="${assets.client.js}" defer></script>`
-            : `<script src="${assets.client.js}" defer crossorigin></script>`}
+        ${
+          assets.client.css
+            ? `<link rel="stylesheet" href="${assets.client.css}">`
+            : ''
+        }
+          ${
+            process.env.NODE_ENV === 'production'
+              ? `<script src="${assets.client.js}" defer></script>`
+              : `<script src="${assets.client.js}" defer crossorigin></script>`
+          }
     </head>
     <body>
         <div id="root">${markup}</div>
@@ -59,6 +62,6 @@ server
         </script>
     </body>
 </html>`);
-    });
+  });
 
 export default server;
